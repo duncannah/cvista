cf0e4823fe1ed0f5595c080bb10da61badba9a12
ficelle3 <appleoriginal911@gmail.com>
2023-11-08 16:50:42 +0100

    somewhat less ugly front end for the user info form. Code is a mess though.

--- src/components/EditInfo.jsx
+++ src/components/EditInfo.jsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from "react";
 
+const centered = {
+	"justify-content": "center",
+	"align-items": "middle",
+	"margin": "0.5em auto",
+}
+const centeredInput = {
+	...centered,
+	"padding-left": "0.5rem",
+	"border-radius": "0.9rem",
+	"border": "solid deepskyblue 2px",
+}
+
 
 const EditInfo = (props) => {
 	const [userData, setUserData] = useState({"name": "", "title": "", "email": "", "phone": "", "experience": []});
@@ -46,44 +58,46 @@ const EditInfo = (props) => {
 
 
 	return (
-		<div class="center">
-			<form onChange={updateUser}>
-				<input type="text" value={userData.name || ""} placeholder="your name" name="name"/>
-				<input type="text" value={userData.title || ""} placeholder="your title" name="title"/>
-				<input type="email" value={userData.email || ""} placeholder="your email" name="email"/>
-				<input type="text" value={userData.phone || ""} placeHolder="your phone number" name="phone"/>
+		<div style={centered}>
+			<form style={{...centered, "display": "flex", "flex-direction": "column"}}onChange={updateUser}>
+				<input type="text" value={userData.name || ""} placeholder="your name" name="name" style={{...centeredInput, "width": "50%"}}/>
+				<input type="text" value={userData.title || ""} placeholder="your title" name="title" style={{...centeredInput, "width": "50%"}}/>
+				<input type="email" value={userData.email || ""} placeholder="your email" name="email" style={{...centeredInput, "width": "50%"}}/>
+				<input type="text" value={userData.phone || ""} placeHolder="your phone number" name="phone" style={{...centeredInput, "width": "50%"}}/>
 			</form>
-			<table>
-				<thead>
-					<tr>
-						<th>Entreprise</th>
-						<th>Role</th>
-						<th>date de début</th>
-						<th>date de fin</th>
-						<th>description</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-				{userData.experience.map((experience, index) => (
-					<tr>
-						<td>{experience.company}</td>
-						<td>{experience.role}</td>
-						<td>{experience.startDate}</td>
-						<td>{experience.endDate}</td>
-						<td>{experience.description}</td>
-						<td><button onClick={deleteExperience} value={index}> - </button></td>
-					</tr>
-				))}
-				</tbody>
-			</table>
-			<form onChange={updateExperience}>
-				<input type="text" placeholder="Entreprise" name="company" value={experience.company}/>
-				<input type="text" placeholder="Role" name="role" value={experience.role}/>
-				<input type="text" placeholder="date de début" name="startDate" value={experience.startDate}/>
-				<input type="text" placeholder="date de fin" name="endDate" value={experience.endDate}/>
-				<input type="text" placeholder="description" name="description" value={experience.description}/>
-				<input type="button" value="+" onClick={submitExperience}/>
+			<form onChange={updateExperience} style={centered}>
+				<table style={centered}>
+					<thead>
+						<tr>
+							<th>Entreprise</th>
+							<th>Role</th>
+							<th>date de début</th>
+							<th>date de fin</th>
+							<th>description</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+					{userData.experience.map((experience, index) => (
+						<tr>
+							<td>{experience.company}</td>
+							<td>{experience.role}</td>
+							<td>{experience.startDate}</td>
+							<td>{experience.endDate}</td>
+							<td>{experience.description}</td>
+							<td><button onClick={deleteExperience} value={index} style={{...centeredInput, "width": "1.5rem", "padding": "0.5rem"}}> - </button></td>
+						</tr>
+					))}
+						<tr>
+							<td><input type="text" placeholder="Entreprise" name="company" value={experience.company} style={centeredInput}/></td>
+							<td><input type="text" placeholder="Role" name="role" value={experience.role} style={centeredInput}/></td>
+							<td><input type="text" placeholder="date de début" name="startDate" value={experience.startDate} style={centeredInput}/></td>
+							<td><input type="text" placeholder="date de fin" name="endDate" value={experience.endDate} style={centeredInput}/></td>
+							<td><input type="text" placeholder="description" name="description" value={experience.description} style={centeredInput}/></td>
+							<td><input type="button" value="+" onClick={submitExperience} style={{...centeredInput, "width": "1.5rem", "padding": "0.5rem"}}/></td>
+						</tr>
+					</tbody>
+				</table>
 			</form>
 		</div>
 	);
