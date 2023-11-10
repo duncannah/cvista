02cd8ab42e6b57519797e0d81fa78da9fa7c8cfb
ficelle3 <appleoriginal911@gmail.com>
2023-11-10 09:42:45 +0100

    display edit form side by side with CV, many bugs remain

--- src/components/EditInfo.jsx
+++ src/components/EditInfo.jsx
@@ -39,7 +39,7 @@ const EditInfo = (props) => {
 	const deleteExperience = () => {
 		const newExperiences = [];
 		for (let elt in userData.experience) {
-			if (elt !== event.target.value) {
+			if (elt !== event.target.id) {
 				newExperiences.push(userData.experience[elt])
 			}
 		}
@@ -68,71 +68,109 @@ const EditInfo = (props) => {
 
 
 	return (
-		<div className="flex flex-col m-auto justify-center">
+		<div className={ (props.width === "full" ? "" : "w-1/2" ) + " flex flex-col m-auto justify-center"}>
 			<h3 className="text-lg font-bold">Informations générales</h3>
 			<form className="flex flex-col justify-center w-1/2 m-auto" onChange={updateUser}>
-				<input type="text" value={userData.name || ""} placeholder="your name" name="name" 
+				<input type="text" value={userData.name || ""} onChange={() => {}} placeholder="your name" name="name" 
 					className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"/>
-				<input type="text" value={userData.title || ""} placeholder="your title" name="title" 
+				<input type="text" value={userData.title || ""} onChange={() => {}} placeholder="your title" name="title" 
 					className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100" />
-				<input type="email" value={userData.email || ""} placeholder="your email" name="email" 
+				<input type="email" value={userData.email || ""} onChange={() => {}} placeholder="your email" name="email" 
 					className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100" />
-				<input type="text" value={userData.phone || ""} placeholder="your phone number" name="phone" 
+				<input type="text" value={userData.phone || ""} onChange={() => {}} placeholder="your phone number" name="phone" 
 					className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100" />
 			</form>
 			<h3 className="text-lg font-bold">Experiences</h3>
-			<form onChange={updateExperience} >
-				<table>
-					<thead>
-						<tr>
-							<th>Entreprise</th>
-							<th>Role</th>
-							<th>date de début</th>
-							<th>date de fin</th>
-							<th>description</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-					{userData.experience.map((experience, index) => (
-						<tr>
-							<td>{experience.company}</td>
-							<td>{experience.role}</td>
-							<td>{experience.startDate}</td>
-							<td>{experience.endDate}</td>
-							<td>{experience.description}</td>
-							<td><button onClick={deleteExperience} value={index}
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							> - </button></td>
-						</tr>
-					))}
-						<tr class="border-top border-solid border-1">
-							<td><input type="text" placeholder="Entreprise" name="company" value={experience.company} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-							<td><input type="text" placeholder="Role" name="role" value={experience.role} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-							<td><input type="text" placeholder="date de début" name="startDate" value={experience.startDate} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-							<td><input type="text" placeholder="date de fin" name="endDate" value={experience.endDate} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-							<td><input type="text" placeholder="description" name="description" value={experience.description} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-							<td><input type="button" value="+" onClick={submitExperience} 
-								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-							/></td>
-						</tr>
-					</tbody>
-				</table>
-			</form>
+			{props.width === "full" ? (
+				<form onChange={updateExperience} >
+					<table>
+						<thead>
+							<tr>
+								<th>Entreprise</th>
+								<th>Role</th>
+								<th>date de début</th>
+								<th>date de fin</th>
+								<th>description</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						{userData.experience.map((experience, index) => (
+							<tr>
+								<td>{experience.company}</td>
+								<td>{experience.role}</td>
+								<td>{experience.startDate}</td>
+								<td>{experience.endDate}</td>
+								<td>{experience.description}</td>
+								<td><button onClick={deleteExperience} id={index}
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								> - </button></td>
+							</tr>
+						))}
+							<tr class="border-top border-solid border-1">
+								<td><input type="text" placeholder="Entreprise" name="company" value={experience.company} onChange={() => {}} 
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+								<td><input type="text" placeholder="Role" name="role" value={experience.role} onChange={() => {}} 
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+								<td><input type="text" placeholder="date de début" name="startDate" value={experience.startDate} onChange={() => {}} 
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+								<td><input type="text" placeholder="date de fin" name="endDate" value={experience.endDate}  onChange={() => {}}
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+								<td><input type="text" placeholder="description" name="description" value={experience.description} onChange={() => {}} 
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+								<td><input type="button" value="+" onClick={submitExperience} onChange={() => {}} 
+									className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+								/></td>
+							</tr>
+						</tbody>
+					</table>
+				</form>
+			) : (
+				<div>
+					{
+						userData.experience.map((experience, index) => (
+							<div className="flex flex-col rounded-25 transition ease-in-out duration-300 hover:bg-gray-100" id={index} onClick={deleteExperience}>
+								<h4 id={index} className="text-lg font-bold">{experience.company}</h4>
+								<h5 id={index} className="text-md font-bold">{experience.role}</h5>
+								<p id={index} className="text-sm">
+									{experience.startDate} - {experience.endDate}
+								</p>
+								<p id={index} className="text-sm">{experience.description}</p>
+							</div>
+						))
+					}
+					<form onChange={updateExperience} className="flex flex-col">
+						<input type="text" placeholder="Entreprise" name="company" value={experience.company} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+						<input type="text" placeholder="Role" name="role" value={experience.role} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+						<input type="text" placeholder="date de début" name="startDate" value={experience.startDate} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+						<input type="text" placeholder="date de fin" name="endDate" value={experience.endDate} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+						<input type="text" placeholder="description" name="description" value={experience.description} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+						<input type="button" value="+" onClick={submitExperience} onChange={() => {}} 
+							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+						/>
+					</form>
+				</div>
+			)}
 
 			<h3 className="text-lg font-bold">Capacités</h3>
 			<form>
 				<table className="w-1/2 m-auto">
+					<tbody>
 						{userData.skills.map((text, index) => (
 							<tr>
 								<td>{text}</td>
@@ -141,14 +179,15 @@ const EditInfo = (props) => {
 								> - </button></td>
 							</tr>
 						))}
-					<tr>
-						<td><input type="text" onChange={updateSkill} value={skill} placeholder="capacité"
-							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-						/></td>
-						<td><button onClick={submitSkill} 
-							className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
-						> + </button></td>
-					</tr>
+						<tr>
+							<td><input type="text" onChange={updateSkill} value={skill} onChange={() => {}} placeholder="capacité"
+								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+							/></td>
+							<td><button onClick={submitSkill} 
+								className="justify-center p-1 m-2 border-solid border-2 rounded-full transition ease-in-out duration-300 hover:bg-gray-100"
+							> + </button></td>
+						</tr>
+					</tbody>
 
 				</table>
 					
